21f0f1d0462730c33df7e28dbdee9e6255e29874
Lynn Samuelson <lynn.samuelson@gmail.com>
2015-07-21 11:57:21 -0500

    initial index.html and index files

---
+++ main.js
@@ -0,0 +1,38 @@
+/*
+  Create a function that multiplies two numbers
+  passed in as argument. Return the product.
+ */
+
+
+/*
+  Create a function that divides two numbers
+  passed in as argument. Return the quotient.
+ */
+
+
+/*
+  Create a variable to hold the return value of the
+  3-argument function. 
+
+  e.g. var result = myFunction(0, 0, func)
+ */
+
+
+/*
+  Log the result
+ */
+
+ function multiply (num1, num2) {
+ 	return num1 * num2;
+ }
+console.log(multiply(1, 2));
+ function divide (num3, num4) {
+ 	return num3 / num4;
+ }
+
+var result = myFunction(multiply, divide, func) {
+	func(multiply, divide);
+}
+
+
+console.log(result);
